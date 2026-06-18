57fb43262d58fa3c5b016aa8096c656630728047
ysfsyln22 <ysfsyln22@gmail.com>
2026-06-18 12:26:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PROJE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int SAYI1;
+            int SAYI2;
+            string METIN1;
+            string METIN2;
+            int SONUC;
+
+            Console.Write("1. Sayıyı Giriniz : ");
+            METIN1 = Console.ReadLine();
+            SAYI1 = Convert.ToInt32(METIN1);
+
+            Console.Write("2. Sayıyı Giriniz : ");
+            METIN2 = Console.ReadLine();
+            SAYI2 = Convert.ToInt32(METIN2);
+
+            SONUC = SAYI1 + SAYI2;
+
+            Console.WriteLine(SONUC.ToString());
+        }
+    }
+}
